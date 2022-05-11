01e0080ea4874ef62ba0c4831e58a73f29a7a5cf
shoken1011 <nakase-shoken-wd@ynu.jp>
2022-05-11 22:35:27 +0900

    first commit

---
+++ firstapp/src/App.vue
@@ -0,0 +1,19 @@
+
+
+<template>
+  <div>
+    <ClockApp class="clock"/>
+  </div>
+</template>
+
+
+<script>
+import ClockApp from "@/components/ClockApp"
+ 
+ 
+export default {
+  components: {
+    ClockApp
+  },
+}
+</script>
